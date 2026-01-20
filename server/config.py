a71b676c4368587a4e833d7d75a1b39b2303c7f9
Tien190 <hvt1230@gmail.com>
2026-01-20 17:39:18 +0700

    Fix server config

--- server/config.py
+++ server/config.py
@@ -1,25 +1,5 @@
-"""Server entrypoint."""
-import socket
-from server.config import HOST, PORT
-from server.handlers.client_handler import ClientHandler
-from server.handlers.game_handler import MatchMaker
-
-
-def main():
-    token_map = {}
-    match_maker = MatchMaker()
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(100)
-
-    print(f"[SERVER] Listening on {HOST}:{PORT}")
-
-    while True:
-        client_sock, _ = server.accept()
-        handler = ClientHandler(client_sock, match_maker, token_map)
-        handler.start()
-
-
-if __name__ == "__main__":
-    main()
+"""Server configuration."""
+HOST = "0.0.0.0"
+PORT = 5000
+BEST_OF = 3
+RECONNECT_TIMEOUT = 30  # seconds
