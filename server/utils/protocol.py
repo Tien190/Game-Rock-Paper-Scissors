9da29dc541cc814dd6fccbc89a458ac869d070c1
qthang201105 <ptqthang201105@gmail.com>
2026-01-20 16:39:00 +0700

    protocol

--- server/utils/protocol.py
+++ server/utils/protocol.py
@@ -1,1 +1,22 @@
 """Shared protocol helpers."""
+import json
+import threading
+
+
+class ProtocolStream:
+    def __init__(self, sock):
+        self.sock = sock
+        self.file = sock.makefile("rwb")
+        self.lock = threading.Lock()
+
+    def send(self, data: dict):
+        payload = (json.dumps(data) + "\n").encode("utf-8")
+        with self.lock:
+            self.file.write(payload)
+            self.file.flush()
+
+    def recv(self):
+        line = self.file.readline()
+        if not line:
+            return None
+        return json.loads(line.decode("utf-8").strip())
